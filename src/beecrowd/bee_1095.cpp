06168d7e90772baa43c6ffef75d2fa41b301eee3
imrooteodoro <adelsonteodoro67@gmail.com>
2025-05-20 19:28:04 -0300

    feat: add beecrowd 1095 question

---
+++ src/beecrowd/bee_1095.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int il = 1;
+    int j = 60;
+    while (j >= 0)
+    {
+
+        cout<<"I=" << il<< " " <<"J=" <<j << endl;
+        il +=3;
+        j-=5;
+
+
+    }
+    
+
+    return 0;
+}
