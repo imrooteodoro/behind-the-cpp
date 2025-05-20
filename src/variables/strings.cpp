701a851cb357ec385a992d4d022e339da3353d85
imrooteodoro <adelsonteodoro67@gmail.com>
2025-05-20 16:02:43 -0300

    feat: add strings function

---
+++ src/variables/strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string name = "Adelson";
+    char initial_name = 'A';
+
+    cout << name + "\n" + initial_name;
+
+    return 0;
+
+}
